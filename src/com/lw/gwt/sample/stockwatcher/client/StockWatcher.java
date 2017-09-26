c9061da5c267461e28a9b636d833809424ad909f
unknown <e.karachun@ws-potapovich.logic-way.local>
2017-09-26 18:25:52 +0300

    added enter keydown event handler

--- src/com/lw/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/lw/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,8 @@
 package com.lw.gwt.sample.stockwatcher.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
 public class StockWatcher implements EntryPoint {
@@ -27,5 +29,28 @@ public class StockWatcher implements EntryPoint {
 
         RootPanel.get("stockList").add(mainPanel);
         newSymbolTextBox.setFocus(true);
+
+        addStockButton.addClickHandler(e -> addStock());
+
+        newSymbolTextBox.addKeyDownHandler(e -> {
+           if(e.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+               addStock();
+           }
+        });
+
+    }
+
+    private void addStock() {
+        final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+        newSymbolTextBox.setFocus(true);
+
+        // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
+        if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+            Window.alert("'" + symbol + "' is not a valid symbol.");
+            newSymbolTextBox.selectAll();
+            return;
+        }
+
+        newSymbolTextBox.setText("");
     }
 }
