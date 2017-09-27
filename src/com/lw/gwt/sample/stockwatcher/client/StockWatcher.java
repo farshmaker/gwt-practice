89ade7f74c8c28bb1ea8c686c4da9db6dd04caea
unknown <e.karachun@ws-potapovich.logic-way.local>
2017-09-27 17:53:19 +0300

    added all underlying client-side functionality

--- src/com/lw/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/lw/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -2,16 +2,27 @@ package com.lw.gwt.sample.stockwatcher.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class StockWatcher implements EntryPoint {
+    private static final int REFRESH_INTERVAL = 1000;
+
     private VerticalPanel mainPanel = new VerticalPanel();
     private HorizontalPanel addPanel = new HorizontalPanel();
     private FlexTable stocksFlexTable = new FlexTable();
     private TextBox newSymbolTextBox = new TextBox();
     private Button addStockButton = new Button("Add");
     private Label lastUpdatedLabel = new Label();
+    private List<String> stocks = new ArrayList<String>();
 
 
     public void onModuleLoad() {
@@ -33,10 +44,18 @@ public class StockWatcher implements EntryPoint {
         addStockButton.addClickHandler(e -> addStock());
 
         newSymbolTextBox.addKeyDownHandler(e -> {
-           if(e.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-               addStock();
-           }
+            if (e.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+                addStock();
+            }
         });
+
+        Timer refreshTimer = new Timer() {
+            @Override
+            public void run() {
+                refreshWatchList();
+            }
+        };
+        refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
 
     }
 
@@ -51,6 +70,65 @@ public class StockWatcher implements EntryPoint {
             return;
         }
 
+        if (stocks.contains(symbol))
+            return;
+
+        int row = stocksFlexTable.getRowCount();
+        stocks.add(symbol);
+        stocksFlexTable.setText(row, 0, symbol);
+
+        Button removeStockButton = new Button("x");
+        removeStockButton.addClickHandler(e -> {
+            int removedIndex = stocks.indexOf(symbol);
+            stocks.remove(removedIndex);
+            stocksFlexTable.removeRow(removedIndex + 1);
+        });
+        stocksFlexTable.setWidget(row, 3, removeStockButton);
+
+        refreshWatchList();
+
         newSymbolTextBox.setText("");
+    }
+
+    private void refreshWatchList() {
+        final double MAX_PRICE = 100.0;
+        final double MAX_PRICE_CHANGE = 0.02;
+
+        StockPrice[] prices = new StockPrice[stocks.size()];
+        for (int i = 0; i < stocks.size(); i++) {
+            double price = Random.nextDouble() * MAX_PRICE;
+            double change = price * MAX_PRICE_CHANGE * (Random.nextDouble() * 2.0 - 1.0);
+
+            prices[i] = new StockPrice(stocks.get(i), price, change);
+        }
+
+        updateTable(prices);
+    }
+
+    private void updateTable(StockPrice[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            updateTable(prices[i]);
+        }
+        System.out.println("");
+
+        DateTimeFormat dateFormat = DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM);
+        lastUpdatedLabel.setText("Last update : " + dateFormat.format(new Date()));
+    }
+
+    private void updateTable(StockPrice price) {
+        if (!stocks.contains(price.getSymbol())) {
+            return;
+        }
+
+        int row = stocks.indexOf(price.getSymbol()) + 1;
+
+        String priceText = NumberFormat.getFormat("#,##0.00").format(
+                price.getPrice());
+        NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
+        String changeText = changeFormat.format(price.getChange());
+        String changePercentText = changeFormat.format(price.getChangePercent());
+
+        stocksFlexTable.setText(row, 1, priceText);
+        stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText + "%)");
     }
 }
